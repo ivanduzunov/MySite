183f22339251ca0c1d230f6a6ca2f1053f6387d4
ivanduzunov <ivan.d.uzunov@gmail.com>
2018-07-25 20:12:56 +0300

    ...

--- MySite/MySite.Web/wwwroot/js/homePage.js
+++ MySite/MySite.Web/wwwroot/js/homePage.js
@@ -1,26 +1,32 @@
 ﻿$(document).ready(function () {
     $('#welcomeDiv').show();
-    let counter = 5000;
+    let counter = 1000;
     $(".titleText").each(function () {
         $(this).fadeIn(counter);
-        counter += 5000;
+        counter += 1000;
     });
 
-    $("#image").fadeIn(25000);
+    $("#image").fadeIn(6000);
 });
 
 function enterSite() {
+    let counter = 0;
     let img = $("#image");
     let nav = $('#navbar');
+    let titleTextArr = $(".titleText");
     let currentHeight = $('#welcomeDiv').css('height');
     $('#welcomeDiv').css('height', currentHeight);
 
-    $(".titleText").each(function () {
-        $(this).fadeOut();
+    titleTextArr.each(function () {
+        counter++
+        $(this).fadeOut(1000);
+        if (counter === 3) {
+            img.delay(1000).animate({ marginLeft: '-=900px', marginTop: "+=20px", height: '50px', width: '50px' }, 2000, function () {
+                nav.fadeIn(3000);
+            });
+        }
     });
 
-    img.animate({ height: '50px', width: '50px' }, 1000);
+   
 
-    nav.fadeIn(3000);
-      
 };
