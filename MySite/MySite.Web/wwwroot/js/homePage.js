0fac603729f55aa1b00b50e5f530d7f73125ef75
ivanduzunov <ivan.d.uzunov@gmail.com>
2018-07-26 20:23:57 +0300

    ...

--- MySite/MySite.Web/wwwroot/js/homePage.js
+++ MySite/MySite.Web/wwwroot/js/homePage.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿
+$(document).ready(function () {
     $('#welcomeDiv').show();
     let counter = 1000;
     $(".titleText").each(function () {
@@ -9,24 +10,24 @@
     $("#image").fadeIn(6000);
 });
 
-function enterSite() {
-    let counter = 0;
-    let img = $("#image");
-    let nav = $('#navbar');
-    let titleTextArr = $(".titleText");
-    let currentHeight = $('#welcomeDiv').css('height');
-    $('#welcomeDiv').css('height', currentHeight);
+function enterSite(isInWelcomePage) {
 
-    titleTextArr.each(function () {
-        counter++
-        $(this).fadeOut(1000);
-        if (counter === 3) {
-            img.delay(1000).animate({ marginLeft: '-=900px', marginTop: "+=20px", height: '50px', width: '50px' }, 2000, function () {
-                nav.fadeIn(3000);
+    if (isInWelcomePage) {
+        let img = $("#image");
+        let nav = $('#navbar');
+        let titleTextArr = $(".titleText");
+        let currentHeight = $('#welcomeDiv').css('height');
+        $('#welcomeDiv').css('height', currentHeight);
+
+        img.animate({ height: '0px', width: '0px' }, 2000, function () {
+            titleTextArr.each(function () {
+                $(this).delay(2000).fadeOut(1000, function () {
+                    img.hide();
+                    nav.fadeIn(3000);
+                });
             });
-        }
-    });
-
+        });
+    } 
    
-
 };
+
